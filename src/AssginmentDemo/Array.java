ee13e25536160ad11f870b936b92ad7ce8b7ac58
MithunBurman7 <mithunburman7@gmail.com>
2017-06-30 14:51:13 +0530

    Tried to print the value of 6th element

---
+++ src/AssginmentDemo/Array.java
@@ -0,0 +1,12 @@
+package AssginmentDemo;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// Here I'm declaring int array, tried to print the value of 6th element 
+     int[] array = { 2,4,5,7,8 };
+   
+     System.out.println(array[5]);
+   }
+
+}
